e7ed6c4544a7f7dbb35216d8f397636d95a12925
juan.mejia <juan.mejia@wirecard.com>
2018-11-27 16:31:40 -0400

    starting to work with sessions

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
 const bodyParser = require('body-parser');
 // const rootDir = require('./util/path');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express();
 
@@ -13,11 +15,18 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+    secret: 'my retsec',
+    resave: false,
+    saveUninitialized: false
+}))
+
 app.use('/users', usersRouter.router);
 app.use('/products', productsRouter.router);
 
 app.use('/', function(req, res, next) {
     // res.sendFile(path.join(rootDir, 'views', 'home.html'));
+    // req.session.isLogged = false;
     res.render('home', { pageTitle: 'Home' });
 });
 
